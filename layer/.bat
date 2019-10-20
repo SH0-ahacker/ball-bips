8a01b3aac6f668f1338c87fa7f8eb32d099abd43
lagalapizza_1011 <52937131+SH0-ahacker@users.noreply.github.com>
2019-10-20 20:58:52 -0300

    Create .bat

---
+++ layer/.bat
@@ -0,0 +1,11 @@
+@echo off
+TITLE info
+pause
+echo: this part is a layer
+pause
+cls
+echo: don´t make a alteration in this code
+pause
+cls
+echo: motive: a error in this code = 1.00000.0000.000.00.0 errors = giant chance of a BSoD displays
+pause
